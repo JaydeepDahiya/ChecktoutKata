d87b019d51d49108851f6eeff6dbfc4805613303
Jaydeep Dahiya <jaydeep.dahiya@gmail.com>
2021-01-26 20:06:32 +0000

    Added new tests for checking pricing rules

---
+++ src/test/java/com/jaydeepdahiya/PricingRuleTest.java
@@ -0,0 +1,32 @@
+package com.jaydeepdahiya;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.jaydeepdahiya.model.Offer;
+import com.jaydeepdahiya.model.PricingRule;
+
+class PricingRuleTest {
+
+    @Test
+    void testPricingRuleWithoutOffer() {
+        PricingRule pricingRule = new PricingRule("A", 30);
+
+        assertEquals(0, pricingRule.priceForQuantity(0));
+        assertEquals(150, pricingRule.priceForQuantity(5));
+        assertEquals(300, pricingRule.priceForQuantity(10));
+    }
+
+    @Test
+    void testPricingRuleWithOffer() {
+        PricingRule pricingRule = new PricingRule("A", 50, new Offer(3, 120));
+
+        assertEquals(50, pricingRule.priceForQuantity(1));
+        assertEquals(100, pricingRule.priceForQuantity(2));
+        assertEquals(120, pricingRule.priceForQuantity(3));
+        assertEquals(170, pricingRule.priceForQuantity(4));
+        assertEquals(240, pricingRule.priceForQuantity(6));
+    }
+
+}
