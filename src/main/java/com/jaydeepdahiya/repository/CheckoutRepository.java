f444cd47af54bd2835b95a702cb01c426e0a87bb
Jaydeep Dahiya <jaydeep.dahiya@gmail.com>
2021-01-26 19:46:11 +0000

    Updated reposotiry forreposotory to enable value being set for testing

--- src/main/java/com/jaydeepdahiya/repository/CheckoutRepository.java
+++ src/main/java/com/jaydeepdahiya/repository/CheckoutRepository.java
@@ -6,8 +6,16 @@ import com.jaydeepdahiya.model.PricingRule;
 
 public class CheckoutRepository {
 
+	private Set<PricingRule> pricingRules;
+
+	// This method should ideally get the pricing rules from database and then send back the values
+	public Set<PricingRule> getPricingRules() {
+		return pricingRules;
+	}
+
+	// Only implemented for testing purpose to initialize the values
 	public void setPricingRules(Set<PricingRule> pricingRules) {
-		// TODO Auto-generated method stub		
+		this.pricingRules = pricingRules;
 	}
 
 }
